82bc85de60eaed9256776f600772345dc193280e
BuntyOnTheBeat <126576031+BuntyOnTheBeat@users.noreply.github.com>
2023-03-03 08:29:38 +0800

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,19 @@
+import random
+
+highest = 10
+answer = random.randint(1, highest)
+print("Please guess a number between 1 and {}:".format(highest))
+
+guess = int(input())
+
+if guess == answer:
+    print("Well done, first try")
+
+while guess != answer:
+    if guess < answer:
+        print("You guessed to low")
+    if guess > answer:
+        print("You guessed to high")
+    if guess == answer:
+        print("You guessed correctly")
+    guess = int(input())
